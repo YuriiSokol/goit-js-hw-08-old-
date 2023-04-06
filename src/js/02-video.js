cc2013d522b86b76e3db37a56c4808d669d390df
YuriiSokol <yura357117@gmail.com>
2023-04-06 19:26:44 +0200

    2 and 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,35 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+
+const onPlay = function (data) {
+  console.log(data.seconds);
+};
+
+const savePlayerTime = data => {
+  console.log(data.seconds);
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+
+player.on('timeupdate', throttle(savePlayerTime, 1000));
+player.on('ended', () => {
+  localStorage.removeItem('videoplayer-current-time');
+  player.unload();
+});
+
+const getLocalTime = () => {
+  const savedTime = localStorage.getItem('videoplayer-current-time');
+  if (savedTime === 0) return 0;
+
+  const parsedTime = JSON.parse(savedTime);
+  return Number(parsedTime);
+};
+const resumePlayerOnLastTime = () => {
+  const lastPlayedTime = getLocalTime();
+  player.setCurrentTime(lastPlayedTime);
+};
+
+resumePlayerOnLastTime();
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,69 @@
+import throttle from 'lodash.throttle';
+const form = document.querySelector('form');
+const inputEmail = form.querySelector('label > input');
+const inputMessage = form.querySelector('label > textarea');
+const submitBtn = form.querySelector('button');
 
+const setSubmitBtnStatus = () => {
+  if (inputMessage.value === '' || inputEmail.value === '') {
+    submitBtn.disabled = true;
+  }
+  if (inputMessage.value != '' && inputEmail.value != '') {
+    submitBtn.disabled = false;
+  }
+};
+
+//checking local-storage for 'feedback-form-state' exist if dont create it empty
+const initializeLocalStorage = () => {
+  if (localStorage.getItem('feedback-form-state') === null) {
+    localStorage.setItem(
+      'feedback-form-state',
+      JSON.stringify({ email: '', message: '' })
+    );
+  }
+};
+initializeLocalStorage();
+
+//updating form on reload
+const localStorageData = JSON.parse(
+  localStorage.getItem('feedback-form-state')
+);
+
+const formUpdateOnstart = localStorageData => {
+  if (localStorageData.email != '') {
+    inputEmail.value = localStorageData.email;
+  }
+  if (localStorageData.message != '') {
+    inputMessage.value = localStorageData.message;
+  }
+  setSubmitBtnStatus();
+};
+formUpdateOnstart(localStorageData);
+
+//
+const data = {
+  email: localStorageData.email,
+  message: localStorageData.message,
+};
+
+// adding input data to object on input event
+const formList = form.addEventListener('input', event => {
+  data[event.target.name] = event.target.value;
+  setSubmitBtnStatus();
+  localStorageUpdate();
+});
+
+// local storage-set throttle 500ms
+const localStorageUpdate = throttle(() => {
+  localStorage.setItem('feedback-form-state', JSON.stringify(data));
+}, 500);
+
+// submit btn
+submitBtn.addEventListener('click', event => {
+  event.preventDefault();
+  inputMessage.value = '';
+  inputEmail.value = '';
+  setSubmitBtnStatus();
+  localStorage.removeItem('feedback-form-state');
+  console.log(data);
+});
